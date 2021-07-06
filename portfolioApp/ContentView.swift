9317fdc28ffcc0e25461821176fb3451cd20d337
Matthhew Pters <mattsterp@gmail.com>
2021-07-06 09:38:42 -0400

    Initial Commit

---
+++ portfolioApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  portfolioApp
+//
+//  Created by Matthew Peters on 7/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ portfolioApp/portfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfolioAppApp.swift
+//  portfolioApp
+//
+//  Created by Matthew Peters on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct portfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
